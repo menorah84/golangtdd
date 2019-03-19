4a67c9fd48a58e96942efc5b44e3f1e7f96bce42
JD Bacolod <menorah84@yahoo.com>
2019-03-19 19:30:38 +0800

    Structs part 1

---
+++ structs/shapes.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
--- sum/sum.go
+++ sum/sum.go
@@ -12,11 +12,17 @@ func Sum(numbers []int) int {
 	
 }
 
-func SumAll(numbersToSum ...[]int) []int {
+func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	
 	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers))
+	
+		if len(numbers) == 0{
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	
 	return sums
